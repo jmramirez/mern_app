408c856797709437f2e5947dac4e42a4258d6c93
Jose Ramirez <josemramirez81@outlook.com>
2019-07-15 20:13:29 -0400

    Users app Backend

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.text())
+
+app.get('/',(request, response, next)=>{
+  response.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Test</title>
+  </head>
+  <body>
+    <div role="application">
+      <form method="post" action="setdata">
+        <input name="urlencoded" type="text" />
+        <button type="submit">Send</button>
+      </form>
+      <form method="post" action="setdata" enctype="text/plain">
+        <input name="txtencoded" type="text" />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  </body>
+  </html>
+  `)
+})
+
+app.post('/setdata' , (req,res,next)=>{
+  console.log(req.body)
+  res.end()
+})
+
+app.listen(1337,()=>console.log('SErver running on port 1337'))
---
+++ template.js
@@ -0,0 +1,13 @@
+export default () => {
+  return `<!DOCTYPE html>
+  <html>
+    <head><meta charset="utf-8">
+    <title>MERN SKeleton</title>
+    </head>
+    <body>
+    <div id="root">Hello World</div>
+    </body>
+  </html>
+  
+  `
+}
